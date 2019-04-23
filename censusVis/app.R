13b330ecf79099e290694ab8eab40ba8caed24fc
Shafi Rubbani <shafirubbani@gmail.com>
2019-04-23 13:43:15 -0400

    Edited sidebar panel

--- censusVis/app.R
+++ censusVis/app.R
@@ -13,16 +13,28 @@ library(shiny)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("censusVis"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+        helpText("Create demographic maps with 
+               information from the 2010 US Census."),
+        
+        selectInput("var", 
+                    label = "Choose a variable to display",
+                    choices = c("Percent White", 
+                                "Percent Black",
+                                "Percent Hispanic", 
+                                "Percent Asian"),
+                    selected = "Percent White"),
+        
+        sliderInput("range",
+                    "Range of interest:",
+                    min = 0,
+                    max = 100,
+                    value = c(0,100),
+                    dragRange = TRUE)
       ),
       
       # Show a plot of the generated distribution
